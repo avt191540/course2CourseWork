d841648c393c5509ee653b10f87d90e7f47e05b7
Igor <avt191540@yandex.ru>
2022-02-05 22:42:57 +0300

    Initial commit

---
+++ src/main/java/pro/sky/course2CourseWork/Course2CourseWorkApplication.java
@@ -0,0 +1,14 @@
+package pro.sky.course2CourseWork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Course2CourseWorkApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(Course2CourseWorkApplication.class, args);
+	}
+
+}
